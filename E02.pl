438b8dc1b1f5024018615d15cdb27fe494004c2a
Michael Hendricks <michael@ndrix.org>
2005-06-01 23:39:22 +0000

    Improved the translation of E02.p6 into perl5
    git-svn-id: svn://tameion/projects/Text-TypingEffort/trunk@762 5cb99f66-e806-0410-bcd3-b1d9210a1b92

--- E02.pl
+++ E02.pl
@@ -1,16 +1,19 @@
+#!/usr/bin/perl
+# bintree - binary tree demo program 
+# adapted from "Perl Cookbook", Recipe 11.15
 use strict;
 use warnings;
 my ($root, $n);
 while ($n++ < 20) { insert($root, int rand 1000) }
-my ($pre, $in, $post) = (0..2);
-print "Pre order:  "; show($root,$pre);  print " \n";
-print "In order:   "; show($root,$in);   print " \n";
-print "Post order: "; show($root,$post); print " \n";
-for (print "Search? "; <$ARGS>; print "Search? ") {
+use constant { PRE => 0, IN => 1, POST => 2 };
+print "Pre order:  "; show($root,PRE);  print "\n";
+print "In order:   "; show($root,IN);   print "\n";
+print "Post order: "; show($root,POST); print "\n";
+for (print "Search? "; <>; print "Search? ") {
     chomp;
     if (my $node = search($root, $_)) {
-        print "Found $_ at $node: $node{VALUE}\n";
-        print "(again!)\n" if $node{FOUND} > 1;
+        print "Found $_ at $node: $node->{VALUE}\n";
+        print "(again!)\n" if $node->{FOUND} > 1;
     }
     else {
         print "No $_ in tree\n";
@@ -19,28 +22,32 @@ for (print "Search? "; <$ARGS>; print "Search? ") {
 exit;
 #########################################
 sub insert {
-    unless (@_[0]) {
-        @_[0] = { LEFT  => undef, RIGHT => undef,
-            VALUE => @_[1], FOUND => 0,
-        };
+    unless ($_[0]) {
+        my %node;
+        $node{LEFT}   = undef;
+        $node{RIGHT}  = undef;
+        $node{VALUE}  = $_[1]; $node{FOUND} = 0;
+        $_[0] = \%node;
         return;
     }
-    if    (@_[0]{VALUE} > @_[1]) { insert(@_[0]{LEFT},  @_[1]) }
-    elsif (@_[0]{VALUE} < @_[1]) { insert(@_[0]{RIGHT}, @_[1]) }
-    else                         { warn "dup insert of @_[1]\n"  }
+    my ($tree,$val) = @_;
+    if    ($tree->{VALUE} > $val) { insert($tree->{LEFT},  $val) }
+    elsif ($tree->{VALUE} < $val) { insert($tree->{RIGHT}, $val) }
+    else                          { warn "dup insert of $val\n"  }
 }
 sub show  {
-    return unless @_[0];
-    show(@_[0]{LEFT}, @_[1]) unless @_[1] == $post;
-    show(@_[0]{RIGHT},@_[1])     if @_[1] == $pre;
-    print @_[0]{VALUE};
-    show(@_[0]{LEFT}, @_[1])     if @_[1] == $post;
-    show(@_[0]{RIGHT},@_[1]) unless @_[1] == $pre;
+    return unless $_[0];
+    show($_[0]{LEFT}, $_[1]) unless $_[1] == POST;
+    show($_[0]{RIGHT},$_[1])     if $_[1] == PRE;
+    print $_[0]{VALUE};
+    show($_[0]{LEFT}, $_[1])     if $_[1] == POST;
+    show($_[0]{RIGHT},$_[1]) unless $_[1] == PRE;
 }
 sub search {
-    return unless @_[0];
-    return search(@_[0]{@_[1]<@_[0]{VALUE} && "LEFT" || "RIGHT"}, @_[1])
-    unless @_[0]{VALUE} == @_[1];
-    @_[0]{FOUND}++;
-    return @_[0];
+    my $tree = shift;
+    return unless $tree;
+    return search($tree->{$_[0]<$tree->{VALUE} && "LEFT" || "RIGHT"}, $_[0])
+        unless $tree->{VALUE} == $_[0];
+    $tree->{FOUND}++;
+    return $tree;
 }
