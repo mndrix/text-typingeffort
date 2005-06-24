7601c47fa25d15ca78fff0870801cfb994ccf4a0
Michael Hendricks <michael@ndrix.org>
2005-06-24 01:44:22 +0000

    Removed the to-do note about the ASET keyboard layout
    git-svn-id: svn://tameion/projects/Text-TypingEffort/trunk@808 5cb99f66-e806-0410-bcd3-b1d9210a1b92

--- lib/Text/TypingEffort.pm
+++ lib/Text/TypingEffort.pm
@@ -251,11 +251,6 @@ Count the unrecognized characters
 
 =item *
 
-Add support for the 'aset' keyboard which is like QWERTY, but has the 
-dfjk keys swapped with the etni keys.
-
-=item *
-
 Allow the user to specify custom keyboard layouts
 
 =item *
